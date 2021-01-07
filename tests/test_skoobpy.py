a334923fa091b375cd9c098ba3f1691d7fe0faa0
Diegoslourenco <diego.lourenco15@gmail.com>
2021-01-07 18:26:20 -0300

    add function filter_currently_reading_books

--- tests/test_skoobpy.py
+++ tests/test_skoobpy.py
@@ -20,12 +20,21 @@ def total_desired_books():
     all_books = get_all_books(user_id)
     return filter_desired_books(all_books)
 
+@pytest.fixture
+def total_currently_reading_books():
+    user_id = 1380619
+    all_books = get_all_books(user_id)
+    return filter_currently_reading_books(all_books)
+
 # Tests
 def test_total_books(total_books):
     assert len(total_books) == 619
 
 def test_total_desired_books(total_desired_books):
     assert len(total_desired_books) == 466
+
+def test_total_currently_reading_books(total_currently_reading_books):
+    assert len(total_currently_reading_books) == 3
 
 
 
